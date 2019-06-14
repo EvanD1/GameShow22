c7a7f00ed0d7587211228f42eb3448a0c567574a
Your Name <you@example.com>
2019-06-14 12:56:48 -0600

    finished project

--- GameShow2/ViewController.swift
+++ GameShow2/ViewController.swift
@@ -65,7 +65,7 @@ class ViewController: UIViewController {
                 
                 
                 buttonpressed = "green"
-                
+            
                 
                 readyPlayer1()
                 
